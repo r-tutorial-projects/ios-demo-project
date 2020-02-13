ac4a48e8e99f050e62d5cc656d79071fe8f49897
Ruben Vitt <r.vitt@fme.de>
2020-02-13 18:08:51 +0100

    init(project)

---
+++ Demo Project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Demo Project
+//
+//  Created by Ruben Vitt on 07.02.20.
+//  Copyright © 2020 Rubeen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
